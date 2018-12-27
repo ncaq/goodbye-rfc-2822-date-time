0721d522c56d72898b8914135fd19ab01979806f
ncaq <ncaq@ncaq.net>
2018-12-27 14:24:43 +0900

    added: web-ext-config: srcをパックしない closed #2

---
+++ web-ext-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  build: {
+    overwriteDest: true
+  },
+
+  ignoreFiles: [
+    "src",
+    "README.md",
+    "package.json",
+    "tsconfig.json",
+    "tslint.json",
+    "web-ext-config.js",
+    "yarn.lock"
+  ]
+};

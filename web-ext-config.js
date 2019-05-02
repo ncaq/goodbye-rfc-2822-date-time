c35297e140fb5ee4a81a20067d5de7d8747c498f
ncaq <ncaq@ncaq.net>
2019-05-02 14:32:57 +0900

    modified: tslint to eslint

--- web-ext-config.js
+++ web-ext-config.js
@@ -2,13 +2,11 @@ module.exports = {
   build: {
     overwriteDest: true
   },
-
   ignoreFiles: [
     "src",
     "README.md",
     "package.json",
     "tsconfig.json",
-    "tslint.json",
     "web-ext-config.js",
     "yarn.lock"
   ]

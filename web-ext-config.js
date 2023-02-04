30a46de969765829d6da81134a4843c12d0dbc2f
ncaq <ncaq@ncaq.net>
2023-02-04 22:34:54 +0900

    build: web-ext-config.jsをまとめて、不要な指定も消す

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  verbose: true,
   build: {
-    overwriteDest: true
+    overwriteDest: true,
+    filename: "goodbye-rfc-2822-date-time.zip",
   },
   ignoreFiles: [
     "README.md",
@@ -8,6 +10,10 @@ module.exports = {
     "src",
     "tsconfig.json",
     "web-ext-config.js",
-    "yarn.lock"
-  ]
+    "yarn.lock",
+  ],
+  run: {
+    devtools: true,
+    firefoxProfile: "goodbye-rfc-2822-date-time",
+  },
 };
